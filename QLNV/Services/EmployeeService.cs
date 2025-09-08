7b7750267a10c72b9f15b65e3c3a389198ad74f3
thinh <thinhqn999@gmail.com>
2025-09-08 22:34:15 +0700

    quản lý nhân viên, GetAllEmployees

--- QLNV/Services/EmployeeService.cs
+++ QLNV/Services/EmployeeService.cs
@@ -1,4 +1,6 @@
-﻿using QLNV.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using QLNV.Data;
+using QLNV.ViewModels;
 
 namespace QLNV.Services
 {
@@ -13,7 +15,19 @@ namespace QLNV.Services
       _db = db;
     }
 
-
-
+    // tạo task trong này
+    // s ếch
+    public async Task<List<EmployeeViewModel>> GetAllEmployees()
+    {
+      // linq theo kiểu method syntax
+      return await _db.Employees
+        .OrderBy(x => x.FullName)
+        .Select(x => new EmployeeViewModel
+        {
+          FullName = x.FullName,
+          Department = x.Department,
+          DateOfBirth = x.DateOfBirth,
+        }).ToListAsync();
+    }
   }
 }
